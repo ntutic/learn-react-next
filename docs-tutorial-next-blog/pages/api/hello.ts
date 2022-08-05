0f18de6a9b564636f8fc0a88bc3a9e1d5462c373
Nikola <nikola.tutic@usherbrooke.ca>
2022-08-04 23:09:16 -0400

    Fixed hello.ts per ESLint

--- docs-tutorial-next-blog/pages/api/hello.ts
+++ docs-tutorial-next-blog/pages/api/hello.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 // req = HTTP incoming message, res = HTTP server response
-export default (_: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({ text: 'Hello, world!' });
-  }
+function App(_: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ text: 'Hello, world!' });
+}
+
+export default App;
