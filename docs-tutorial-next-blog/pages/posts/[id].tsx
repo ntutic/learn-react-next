d45acb442c1de99a5f430a613c3b626d86da219c
Nikola <nikola.tutic@usherbrooke.ca>
2022-08-04 23:02:16 -0400

    Migrated to TypeScript

--- docs-tutorial-next-blog/pages/posts/[id].tsx
+++ docs-tutorial-next-blog/pages/posts/[id].tsx
@@ -3,10 +3,10 @@ import Date from '../../components/date';
 import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import { GetStaticPaths, GetStaticProps } from 'next';
 
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
   return {
     props: {
       postData,
@@ -14,7 +14,7 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -22,7 +22,15 @@ export async function getStaticPaths() {
   }; 
 } 
 
-export default function Post({ postData }) {
+export default function Post({ 
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
     return (
         <Layout> 
             <Head>
